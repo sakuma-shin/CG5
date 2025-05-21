a003e1e9afa10cdd6c08d9fd3bfb134318449628
sakuma-shin <k023g0084@g.neec.ac.jp>
2025-05-21 11:16:24 +0900

    課題途中

--- DirectXGame/Shader.cpp
+++ DirectXGame/Shader.cpp
@@ -1,13 +1,15 @@
 #include "Shader.h"
 #include <cassert>
-#include <d3dcompiler.h>
+#include "MiscUtility.h"
 
-void Shader::Load(const std::wstring& filePath, const std::string& shaderModel) {
+void Shader::Load(const std::wstring& filePath, const std::wstring& shaderModel) {
 	ID3DBlob* shaderBlob = nullptr;
 	ID3DBlob* errorBlob = nullptr;
 
+	std::string mbShaderModel = ConvertString(shaderModel);
+
 	HRESULT hr =
-	    D3DCompileFromFile(filePath.c_str(), nullptr, D3D_COMPILE_STANDARD_FILE_INCLUDE, "main", shaderModel.c_str(), D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION, 0, &shaderBlob, &errorBlob);
+	    D3DCompileFromFile(filePath.c_str(), nullptr, D3D_COMPILE_STANDARD_FILE_INCLUDE, "main", mbShaderModel.c_str(), D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION, 0, &shaderBlob, &errorBlob);
 
 	if (FAILED(hr)) {
 		if (errorBlob) {
@@ -19,6 +21,8 @@ void Shader::Load(const std::wstring& filePath, const std::string& shaderModel) 
 	// 生成したShaderBlobを取っておく
 	blob_ = shaderBlob;
 }
+
+void Shader::LoadDxc(const std::wstring& filePath, const std::wstring& shaderModel) {}
 
 ID3DBlob* Shader::GetBlob() { return blob_; }
 
--- DirectXGame/Shader.h
+++ DirectXGame/Shader.h
@@ -1,11 +1,14 @@
 #pragma once
 #include<string>
 #include<d3d12.h>
+#include <d3dcompiler.h>
+#include<dxcapi.h>
 
 class Shader {
 public:
 	//シェーダーファイルを読み込み、コンパイル済みデータを生成する
-	void Load(const std::wstring& filePath, const std::string& shaderModel);
+	void Load(const std::wstring& filePath, const std::wstring& shaderModel);
+	void LoadDxc(const std::wstring& filePath, const std::wstring& shaderModel);
 
 	//生成したコンパイル済みデータを取得する
 	ID3DBlob* GetBlob();
