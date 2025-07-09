36fb522e2a3dde891dfc2707045590eee502fb64
sakuma-shin <k023g0084@g.neec.ac.jp>
2025-07-09 14:12:26 +0900

    実装完了

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -42,13 +42,14 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 	// ピクセルシェーダーの読み込みとコンパイル
 
-	const int kNumPS = 4;
+	const int kNumPS = 5;
 	Shader ps[kNumPS];
 	const std::wstring PS[kNumPS] = {
 	    L"Resources/shaders/TestPS.hlsl",
 	    L"Resources/shaders/GrayScalePS.hlsl",
 	    L"Resources/shaders/VignettePS.hlsl",
 	    L"Resources/shaders/BoxFilterPS.hlsl",
+		L"Resources/shaders/GaussianFilterPS.hlsl",
 	};
 	// pipelineStateの作成
 	PipelineState pipelineState[kNumPS];
