839376ba57506b9b360a8a8b63a2aa7a806e1b20
sakuma-shin <k023g0084@g.neec.ac.jp>
2025-07-10 16:21:07 +0900

    a

--- DirectXGame/Resources/shaders/RadialBlurPS.hlsl
+++ DirectXGame/Resources/shaders/RadialBlurPS.hlsl
@@ -3,6 +3,9 @@
 Texture2D<float32_t4> gTexture : register(t0); //SRV register=>t
 SamplerState gSampler : register(s0); //Sampler register=>s
 
+Texture2D<float32_t4> gMaskTexture : register(t1); //SRV register=>t
+
+
 
 struct PixelShaderOutPut
 {
